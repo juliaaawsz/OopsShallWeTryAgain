afb17bb96531c9e84d2375679289f5011c05aa57
Julia Wisz <wj87918@mail.harpercollege.edu>
2025-01-27 04:55:20 +0000

    App.java

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,11 +1,34 @@
 package org.example;
+import java.util.Scanner;
 
 public class App {
-  public int getInput() {
-    // Implement your function here
+  public static void main(String[] args){
+    getInput();
+  }
+  public static void getInput(){
+    Scanner scanner = new Scanner(System.in);
+    int userNum;
+    while(true){
+      System.out.println("Please enter a value.");
+      if(scanner.hasNextInt()){
+        userNum=scanner.nextInt();
+        if(userNum >= 25 && userNum <= 150){
+          System.out.println("The value chosen by the user is " +userNum);
+          break;
+        }else{
+          System.out.println("The value you chose is invalid");
+          System.out.println("Please enter a value");
+        }
+      }else{
+        System.out.println("This is an invalid inout, please enter a value");
+        scanner.next();
+      }
+      }
+      scanner.close();
+      }
+      
+    
   }
 
-  public static void main(String[] args) {
-    // Test your function here in a driver program
-  }
-}
+  
+
